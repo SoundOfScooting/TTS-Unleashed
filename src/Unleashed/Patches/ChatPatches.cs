9f148792c18bb2f9a9389195cd78da40f54bd641
SoundOfScooting <soundofsealing@gmail.com>
2026-03-27 10:57:44 -0400

    Combine ChatBBCode, FastCommands

--- src/Unleashed/Patches/ChatPatches.cs
+++ src/Unleashed/Patches/ChatPatches.cs
@@ -3,8 +3,23 @@ using Unleashed.Settings;
 namespace Unleashed.Patches;
 
 [HarmonyPatch]
-static class ChatBBCode
+static class ChatPatches
 {
+	[Setting]
+	static readonly Setting<bool> Enabled = new()
+	{
+		MigrateFrom = [
+			("General", "Enable Fast Commands"), // 0.1.0
+		],
+		Section     = Section.Controls,
+		Key         = "Enable Fast Commands",
+		Default     = true,
+		Description =
+			"""
+			When shift is not held down, the 'Help' control instead starts typing a command in chat.
+			Best used when 'Help' is bound to /.
+			""",
+	};
 	[Setting]
 	static readonly Setting<bool> BBCode = new()
 	{
@@ -17,6 +32,29 @@ static class ChatBBCode
 			For example, you can say `[ff0000]this is a [b]red[/b] message[-]`.
 			"""
 	};
+
+	[HarmonyILManipulator]
+	[HarmonyPatch(typeof(NetworkUI), nameof(NetworkUI.Update))]
+	static void UpdateIL(ILContext il)
+	{
+		var c = new ILCursor(il);
+		c.GotoNext(MoveType.After,
+			x => x.MatchLdstr("Help"),
+			x => x.MatchLdcI4((int) ControlType.Keyboard),
+			x => x.MatchCall(AccessTools.Method(typeof(zInput), nameof(zInput.GetButtonDown)))
+		);
+		c.EmitDelegate(bool(bool helpDown) =>
+		{
+			if (!helpDown || !Enabled.Value || zInput.GetButton("Shift"))
+				return helpDown;
+			if (!UICamera.SelectIsInput())
+			{
+				UIChatInput.Instance.ChatButtonOnClick();
+				UIChatInput.Instance.mInput.value = "/";
+			}
+			return false;
+		});
+	}
 
 	[HarmonyPostfix]
 	[HarmonyPatch(typeof(UIChatInput), nameof(UIChatInput.Start))]
