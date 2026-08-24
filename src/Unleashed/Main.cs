3dd54a977fda9d6823009bb99a834c46c6ccbdf1
SoundOfScooting <soundofsealing@gmail.com>
2026-08-24 09:55:40 -0400

    Move global usings

--- src/Unleashed/Main.cs
+++ src/Unleashed/Main.cs
@@ -1,10 +1,3 @@
-global using HarmonyLib;
-global using Mono.Cecil.Cil;
-global using MonoMod.Cil;
-global using NewNet;
-global using UnityEngine;
-global using Unleashed.Extensions;
-global using Unleashed.Util;
 using BepInEx;
 using BepInEx.Configuration;
 using BepInEx.Logging;
---
+++ src/Unleashed/Using.cs
@@ -0,0 +1,7 @@
+global using HarmonyLib;
+global using Mono.Cecil.Cil;
+global using MonoMod.Cil;
+global using NewNet;
+global using UnityEngine;
+global using Unleashed.Extensions;
+global using Unleashed.Util;
